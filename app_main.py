3888360cd46c8323beea93869c7fcbd5fd3ba480
Zannatul Fardaush Tripty <65339499+Zannatul007@users.noreply.github.com>
2024-05-27 13:56:04 +0600

    Add filename

--- app_main.py
+++ app_main.py
@@ -1,183 +1,147 @@
-# from flask import Flask, render_template, request
-# from PIL import Image
-# import requests
-# from transformers import AutoFeatureExtractor, AutoModelForImageClassification
-# import torch
-# import pandas as pd
-# from fractions import Fraction
-# import os
-# app = Flask(__name__)
+from flask import Flask, render_template, request
+from PIL import Image
+import requests
+from transformers import AutoFeatureExtractor, AutoModelForImageClassification
+import torch
+import pandas as pd
+from fractions import Fraction
+import os
+import time
+app = Flask(__name__)
 
-# # Load model and dataset
-# repo_name = "ZaneHorrible/google-vit-base-patch16-384-batch_16_epoch_4_classes_24"
-# feature_extractor = AutoFeatureExtractor.from_pretrained(repo_name)
-# model = AutoModelForImageClassification.from_pretrained(repo_name)
-# df = pd.read_excel(r"H:\ThesisApp\Nutrient list.xlsx")
-# label2id = {'Bhapa Pitha(ভাপা পিঠা)': 0, 'Biriyani(বিরিয়ানি)': 1, 'Chicken Pulao(মোরগ পোলাও)': 2, 'Chickpease Bhuna(ছোলাভুনা)': 3, 'Egg Curry(ডিমভুনা)': 4, 'Falooda(ফালুদা)': 5, 'Fuchka(ফুচকা)': 6, 'Haleem(হালিম)': 7, 'Jalebi(জিলাপী)': 8, 'Kala Bhuna(কালা ভুনা)': 9, 'Khichuri(খিচুড়ি)': 10, 'Malpua Pitha(মালপুয়া পিঠা)': 11, 'Mustard Hilsa(সরষে ইলিশ)': 12, 'Nakshi Pitha(নকশি পিঠা)': 13, 'Panta Ilish(পান্তা ইলিশ)': 14, 'Patishapta Pitha(পাটিসাপটা)': 15, 'Prawn Malai Curry(চিংড়ি মালাইকারী)': 16, 'Rasgulla(রসগোল্লা)': 17, 'Rose Cookies(ফুলঝুরি পিঠা)': 18, 'Roshmalai(রসমালাই)': 19, 'Shahi Tukra(শাহি টুকরা)': 20, 'Shingara(সিঙ্গারা)': 21, 'Sweet Yogurt(মিষ্টি দই)': 22, 'Tehari(তেহারি)': 23}
-# id2label = {0: 'Bhapa Pitha(ভাপা পিঠা)', 1: 'Biriyani(বিরিয়ানি)', 2: 'Chicken Pulao(মোরগ পোলাও)', 3: 'Chickpease Bhuna(ছোলাভুনা)', 4: 'Egg Curry(ডিমভুনা)', 5: 'Falooda(ফালুদা)', 6: 'Fuchka(ফুচকা)', 7: 'Haleem(হালিম)', 8: 'Jalebi(জিলাপী)', 9: 'Kala Bhuna(কালা ভুনা)', 10: 'Khichuri(খিচুড়ি)', 11: 'Malpua Pitha(মালপুয়া পিঠা)', 12: 'Mustard Hilsa(সরষে ইলিশ)', 13: 'Nakshi Pitha(নকশি পিঠা)', 14: 'Panta Ilish(পান্তা ইলিশ)', 15: 'Patishapta Pitha(পাটিসাপটা)', 16: 'Prawn Malai Curry(চিংড়ি মালাইকারী)', 17: 'Rasgulla(রসগোল্লা)', 18: 'Rose Cookies(ফুলঝুরি পিঠা)', 19: 'Roshmalai(রসমালাই)', 20: 'Shahi Tukra(শাহি টুকরা)', 21: 'Shingara(সিঙ্গারা)', 22: 'Sweet Yogurt(মিষ্টি দই)', 23: 'Tehari(তেহারি)'}
-# def convert_to_numeric(amount):
-#     try:
-#         return float(amount)
-#     except ValueError:
-#         if '<' in amount:
-#             return float(amount[1:])
-#         return amount
-# def decimal_to_fraction(decimal):
-#     fraction = Fraction(decimal).limit_denominator()
-#     if fraction.numerator >= fraction.denominator:
-#         whole_part = fraction.numerator // fraction.denominator
-#         remainder = fraction.numerator % fraction.denominator
-#         if remainder == 0:
-#             return str(whole_part)
-#         else:
-#             return f"{whole_part} {remainder}/{fraction.denominator}"
-#     else:
-#         return str(fraction)
+# Load model and dataset
+repo_name = "ZaneHorrible/google-vit-base-patch16-384-batch_16_epoch_4_classes_24"
+feature_extractor = AutoFeatureExtractor.from_pretrained(repo_name)
+model = AutoModelForImageClassification.from_pretrained(repo_name)
+df = pd.read_excel(r"H:\ThesisApp\Nutrient list.xlsx")
+label2id = {'Bhapa Pitha(ভাপা পিঠা)': 0, 'Biriyani(বিরিয়ানি)': 1, 'Chicken Pulao(মোরগ পোলাও)': 2, 'Chickpease Bhuna(ছোলাভুনা)': 3, 'Egg Curry(ডিমভুনা)': 4, 'Falooda(ফালুদা)': 5, 'Fuchka(ফুচকা)': 6, 'Haleem(হালিম)': 7, 'Jalebi(জিলাপী)': 8, 'Kala Bhuna(কালা ভুনা)': 9, 'Khichuri(খিচুড়ি)': 10, 'Malpua Pitha(মালপুয়া পিঠা)': 11, 'Mustard Hilsa(সরষে ইলিশ)': 12, 'Nakshi Pitha(নকশি পিঠা)': 13, 'Panta Ilish(পান্তা ইলিশ)': 14, 'Patishapta Pitha(পাটিসাপটা)': 15, 'Prawn Malai Curry(চিংড়ি মালাইকারী)': 16, 'Rasgulla(রসগোল্লা)': 17, 'Rose Cookies(ফুলঝুরি পিঠা)': 18, 'Roshmalai(রসমালাই)': 19, 'Shahi Tukra(শাহি টুকরা)': 20, 'Shingara(সিঙ্গারা)': 21, 'Sweet Yogurt(মিষ্টি দই)': 22, 'Tehari(তেহারি)': 23}
+id2label2 = {0: 'Bhapa Pitha(ভাপা পিঠা)', 1: 'Biriyani(বিরিয়ানি)', 2: 'Chicken Pulao(মোরগ পোলাও)', 3: 'Chickpease Bhuna(ছোলাভুনা)', 4: 'Egg Curry(ডিমভুনা)', 5: 'Falooda(ফালুদা)', 6: 'Fuchka(ফুচকা)', 7: 'Haleem(হালিম)', 8: 'Jalebi(জিলাপী)', 9: 'Kala Bhuna(কালা ভুনা)', 10: 'Khichuri(খিচুড়ি)', 11: 'Malpua Pitha(মালপুয়া পিঠা)', 12: 'Mustard Hilsa(সরষে ইলিশ)', 13: 'Nakshi Pitha(নকশি পিঠা)', 14: 'Panta Ilish(পান্তা ইলিশ)', 15: 'Patishapta Pitha(পাটিসাপটা)', 16: 'Prawn Malai Curry(চিংড়ি মালাইকারী)', 17: 'Rasgulla(রসগোল্লা)', 18: 'Rose Cookies(ফুলঝুরি পিঠা)', 19: 'Roshmalai(রসমালাই)', 20: 'Shahi Tukra(শাহি টুকরা)', 21: 'Shingara(সিঙ্গারা)', 22: 'Sweet Yogurt(মিষ্টি দই)', 23: 'Tehari(তেহারি)'}
+id2label = {
+    0: 'Bhapa Pitha',
+    1: 'Biriyani',
+    2: 'Morog Pulao',
+    3: 'Kalachana Bhuna',
+    4: 'Dim Bhuna',
+    5: 'Falooda',
+    6: 'Fuchka',
+    7: 'Haleem',
+    8: 'Jilapi',
+    9: 'Kala Bhuna',
+    10: 'Khichuri',
+    11: 'Malpua Pitha',
+    12: 'Shorshe Ilish',
+    13: 'Nakshi Pitha',
+    14: 'Panta Ilish',
+    15: 'Patishapta Pitha',
+    16: 'Chingri Malai Curry',
+    17: 'Roshogolla',
+    18: 'Fuljhuri Pitha',
+    19: 'Roshmalai',
+    20: 'Shahi Tukra',
+    21: 'Shingara',
+    22: 'Mishti Doi',
+    23: 'Tehari'
+}
 
-# def aggregate_food_info(df, food_class, user_serving_size):
-#     food_data = df[df['Class Name'] == food_class]
+def convert_to_numeric(amount):
+    try:
+        return float(amount)
+    except ValueError:
+        if '<' in amount:
+            return float(amount[1:])
+        return amount
+def decimal_to_fraction(decimal):
+    fraction = Fraction(decimal).limit_denominator()
+    if fraction.numerator >= fraction.denominator:
+        whole_part = fraction.numerator // fraction.denominator
+        remainder = fraction.numerator % fraction.denominator
+        if remainder == 0:
+            return str(whole_part)
+        else:
+            return f"{whole_part} {remainder}/{fraction.denominator}"
+    else:
+        return str(fraction)
 
-#     dataset_serving_size = food_data['Serving Size'].iloc[0]
-#     scaling_factor = user_serving_size / dataset_serving_size
-#     print("Dataset serving size:", dataset_serving_size)
+def aggregate_food_info(df, food_class, user_serving_size):
+    food_data = df[df['Class Name'] == food_class]
 
-#     ingredients = food_data[['Ingredients', 'Ingredients Amount', 'Ingredients Unit']].dropna()
-#     ingredients['Ingredients Adjusted Amount'] = (ingredients['Ingredients Amount'] * scaling_factor).round(2)
-#     ingredients = ingredients[['Ingredients', 'Ingredients Amount','Ingredients Adjusted Amount', 'Ingredients Unit']]
-#     ingredients_list = ingredients.to_dict(orient='records')
+    dataset_serving_size = food_data['Serving Size'].iloc[0]
+    scaling_factor = user_serving_size / dataset_serving_size
+    print("Dataset serving size:", dataset_serving_size)
 
-#     ingredients_df = pd.DataFrame(ingredients_list)
+    ingredients = food_data[['Ingredients', 'Ingredients Amount', 'Ingredients Unit']].dropna()
+    ingredients['Ingredients Adjusted Amount'] = (ingredients['Ingredients Amount'] * scaling_factor).round(2)
+    ingredients = ingredients[['Ingredients', 'Ingredients Amount','Ingredients Adjusted Amount', 'Ingredients Unit']]
+    ingredients_list = ingredients.to_dict(orient='records')
 
-#     nutrients = food_data[['Nutrients', 'Nutrients Amount', 'Nutrients Unit']].dropna()
-#     nutrients['Nutrients Amount'] = nutrients['Nutrients Amount'].apply(convert_to_numeric)
-#     nutrients['Nutrients Adjusted Amount'] = nutrients['Nutrients Amount'] * user_serving_size
-#     nutrients =nutrients[['Nutrients', 'Nutrients Amount', 'Nutrients Adjusted Amount','Nutrients Unit']]
-#     nutrients_list = nutrients.to_dict(orient='records')
-#     nutrients_df = pd.DataFrame(nutrients_list)
+    ingredients_df = pd.DataFrame(ingredients_list)
 
-#     return {
-#         'Ingredients': ingredients_df,
-#         'Nutrients': nutrients_df,
-#         'Serving Size': user_serving_size,
-#         'Dataset Serving Size':dataset_serving_size
-#     }
+    nutrients = food_data[['Nutrients', 'Nutrients Amount', 'Nutrients Unit']].dropna()
+    nutrients['Nutrients Amount'] = nutrients['Nutrients Amount'].apply(convert_to_numeric)
+    nutrients['Nutrients Adjusted Amount'] = (nutrients['Nutrients Amount'] * user_serving_size).round(2)
+    nutrients =nutrients[['Nutrients', 'Nutrients Amount', 'Nutrients Adjusted Amount','Nutrients Unit']]
+    nutrients_list = nutrients.to_dict(orient='records')
+    nutrients_df = pd.DataFrame(nutrients_list)
+
+    return {
+        'Ingredients': ingredients_df,
+        'Nutrients': nutrients_df,
+        'Serving Size': user_serving_size,
+        'Dataset Serving Size':dataset_serving_size
+    }
 
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# # @app.route('/predict', methods=['POST'])
-# # def predict():
-# #     if request.method == 'POST':
-# #         image_url = request.form['image_url']
-# #         serving_size = int(request.form['serving_size'])
-# #         image = Image.open(requests.get(image_url, stream=True).raw)
-# #         encoding = feature_extractor(image.convert("RGB"), return_tensors="pt")
-# #         with torch.no_grad():
-# #             outputs = model(**encoding)
-# #             logits = outputs.logits
-# #         predicted_class_idx = logits.argmax(-1).item()
-# #         predicted_class = model.config.id2label[predicted_class_idx]
-# #         food_info = aggregate_food_info(df, predicted_class, serving_size)
-
-# #         return render_template('result.html', predicted_class=predicted_class, food_info=food_info)
 # @app.route('/predict', methods=['POST'])
 # def predict():
 #     if request.method == 'POST':
+#         image_url = request.form['image_url']
 #         serving_size = int(request.form['serving_size'])
+#         image = Image.open(requests.get(image_url, stream=True).raw)
+#         encoding = feature_extractor(image.convert("RGB"), return_tensors="pt")
+#         with torch.no_grad():
+#             outputs = model(**encoding)
+#             logits = outputs.logits
+#         predicted_class_idx = logits.argmax(-1).item()
+#         predicted_class = model.config.id2label[predicted_class_idx]
+#         food_info = aggregate_food_info(df, predicted_class, serving_size)
+
+#         return render_template('result.html', predicted_class=predicted_class, food_info=food_info)
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        serving_size = int(request.form['serving_size'])
         
-#         image = None
-#         if 'image_url' in request.form and request.form['image_url']:
-#             image_url = request.form['image_url']
-#             image = Image.open(requests.get(image_url, stream=True).raw)
-#         elif 'image_file' in request.files and request.files['image_file']:
-#             image_file = request.files['image_file']
-#             image = Image.open(image_file.stream)
+        image = None
+        if 'image_url' in request.form and request.form['image_url']:
+            image_url = request.form['image_url']
+            image = Image.open(requests.get(image_url, stream=True).raw)
+        elif 'image_file' in request.files and request.files['image_file']:
+            image_file = request.files['image_file']
+            image = Image.open(image_file.stream)
 
 
-#         if image:
-#             encoding = feature_extractor(image.convert("RGB"), return_tensors="pt")
-#             with torch.no_grad():
-#                 outputs = model(**encoding)
-#                 logits = outputs.logits
-#             predicted_class_idx = logits.argmax(-1).item()
-#             predicted_class = model.config.id2label[predicted_class_idx]
-#             food_info = aggregate_food_info(df, predicted_class, serving_size)
+        if image:
+            encoding = feature_extractor(image.convert("RGB"), return_tensors="pt")
+            with torch.no_grad():
+                outputs = model(**encoding)
+                logits = outputs.logits
+            predicted_class_idx = logits.argmax(-1).item()
+            predicted_class = model.config.id2label[predicted_class_idx]
+            food_info = aggregate_food_info(df, predicted_class, serving_size)
+            # Introduce the 10-second delay here
+            time.sleep(10)
 
-#             return render_template('result.html', predicted_class=predicted_class, food_info=food_info ,serving_size = serving_size,dataset_serving_size=food_info['Dataset Serving Size'])
-#         else:
-#             return "No image provided", 400
-# # def predict():
-# #     serving_size = int(request.form['serving_size'])
-
-# #     if 'image_file' in request.files and request.files['image_file'].filename != '':
-# #         image_file = request.files['image_file']
-# #         image = Image.open(image_file)
-# #     elif 'image_url' in request.form and request.form['image_url'].strip() != '':
-# #         image_url = request.form['image_url']
-# #         response = requests.get(image_url, stream=True)
-# #         image = Image.open(response.raw)
-# #     else:
-# #         return "No image provided", 400
-
-# #     encoding = feature_extractor(image.convert("RGB"), return_tensors="pt")
-# #     with torch.no_grad():
-# #         outputs = model(**encoding)
-# #         logits = outputs.logits
-# #     predicted_class_idx = logits.argmax(-1).item()
-# #     predicted_class = model.config.id2label[predicted_class_idx]
-# #     food_info = aggregate_food_info(df, predicted_class, serving_size)
-
-# #     return render_template('result.html', predicted_class=predicted_class, food_info=food_info)
-
-# # # if __name__ == "__main__":
-# # #     port = int(os.environ.get("PORT", 5000))
-# # #     app.run(host='0.0.0.0', port=port)
-# # #     # app.run()
-
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5000, host='0.0.0.0')
+            return render_template('result.html', predicted_class=predicted_class, food_info=food_info ,serving_size = serving_size,dataset_serving_size=food_info['Dataset Serving Size'])
+        else:
+            return "No image provided", 400
 
 
-# # from flask import Flask,render_template,redirect,url_for,request
-# # app = Flask(__name__)
-# # @app.route('/')
-# # def index():
-# #     return "Hello world welcome to flask"
-# # @app.route('/<name>')
-# # def print_name(name):
-# #     return "hello {}".format(name)
-# # @app.route('/index')
-# # def welcome():
-# #     return "welcome to index page"
-# # @app.route('/success/<int:score>')
-# # def success(score):
-# #     return "Congrats you are passed by getting {}".format(score)
-# # @app.route('/failure/<int:score>')
-# # def failure(score):
-# #     return "Alas you are failed by getting {}".format(score)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
 
-# # @app.route('/form',methods=["GET","POST"])
-# # def form():
-# #     if request.method =='GET':
-# #         return render_template('form.html')
-# #     else:
-# #         math = float(request.form['math'])
-# #         history = float(request.form['history'])
-# #         science = float(request.form['science'])
 
-# #         average = (math+history+science)/3
-
-# #         result = ""
-# #         if average>40:
-# #             result = "success"
-# #         else:
-# #             result ="failure"
-        
-# #         return redirect(url_for(result,score = average))
-        
-# #         # return render_template('form.html',score = average)
-# # if __name__ == "__main__":
-# #     app.run(debug=True)
 
---
+++ ensemble.py
@@ -0,0 +1,206 @@
+from flask import Flask, render_template, request
+from PIL import Image
+import requests
+from transformers import AutoFeatureExtractor, AutoModelForImageClassification
+import torch
+import pandas as pd
+from fractions import Fraction
+import os
+import multiprocessing 
+
+app = Flask(__name__)
+id2label = {0: 'Bhapa Pitha(ভাপা পিঠা)', 1: 'Biriyani(বিরিয়ানি)', 2: 'Chicken Pulao(মোরগ পোলাও)', 3: 'Chickpease Bhuna(ছোলাভুনা)', 4: 'Egg Curry(ডিমভুনা)', 5: 'Falooda(ফালুদা)', 6: 'Fuchka(ফুচকা)', 7: 'Haleem(হালিম)', 8: 'Jalebi(জিলাপী)', 9: 'Kala Bhuna(কালা ভুনা)', 10: 'Khichuri(খিচুড়ি)', 11: 'Malpua Pitha(মালপুয়া পিঠা)', 12: 'Mustard Hilsa(সরষে ইলিশ)', 13: 'Nakshi Pitha(নকশি পিঠা)', 14: 'Panta Ilish(পান্তা ইলিশ)', 15: 'Patishapta Pitha(পাটিসাপটা)', 16: 'Prawn Malai Curry(চিংড়ি মালাইকারী)', 17: 'Rasgulla(রসগোল্লা)', 18: 'Rose Cookies(ফুলঝুরি পিঠা)', 19: 'Roshmalai(রসমালাই)', 20: 'Shahi Tukra(শাহি টুকরা)', 21: 'Shingara(সিঙ্গারা)', 22: 'Sweet Yogurt(মিষ্টি দই)', 23: 'Tehari(তেহারি)'}
+
+def probabilities_finder(name,model,encoding,probabilities):
+    print(name+" started")
+    # Make predictions
+    with torch.no_grad():
+        outputs = model(**encoding)
+        logits = outputs.logits
+        probabilities = torch.softmax(logits, dim=1)
+    print(name+" finished")
+    return probabilities
+
+
+
+# class_name = id2label[final_preds[0]]
+# print(class_name)
+
+df = pd.read_excel(r"H:\ThesisApp\Nutrient list.xlsx")
+
+def convert_to_numeric(amount):
+    try:
+        return float(amount)
+    except ValueError:
+        if '<' in amount:
+            return float(amount[1:])
+        return amount
+def decimal_to_fraction(decimal):
+    fraction = Fraction(decimal).limit_denominator()
+    if fraction.numerator >= fraction.denominator:
+        whole_part = fraction.numerator // fraction.denominator
+        remainder = fraction.numerator % fraction.denominator
+        if remainder == 0:
+            return str(whole_part)
+        else:
+            return f"{whole_part} {remainder}/{fraction.denominator}"
+    else:
+        return str(fraction)
+
+def aggregate_food_info(df, food_class, user_serving_size):
+    food_data = df[df['Class Name'] == food_class]
+
+    dataset_serving_size = food_data['Serving Size'].iloc[0]
+    scaling_factor = user_serving_size / dataset_serving_size
+    print("Dataset serving size:", dataset_serving_size)
+
+    ingredients = food_data[['Ingredients', 'Ingredients Amount', 'Ingredients Unit']].dropna()
+    ingredients['Ingredients Adjusted Amount'] = (ingredients['Ingredients Amount'] * scaling_factor).round(2)
+    ingredients = ingredients[['Ingredients', 'Ingredients Amount','Ingredients Adjusted Amount', 'Ingredients Unit']]
+    ingredients_list = ingredients.to_dict(orient='records')
+
+    ingredients_df = pd.DataFrame(ingredients_list)
+
+    nutrients = food_data[['Nutrients', 'Nutrients Amount', 'Nutrients Unit']].dropna()
+    nutrients['Nutrients Amount'] = nutrients['Nutrients Amount'].apply(convert_to_numeric)
+    nutrients['Nutrients Adjusted Amount'] = nutrients['Nutrients Amount'] * user_serving_size
+    nutrients =nutrients[['Nutrients', 'Nutrients Amount', 'Nutrients Adjusted Amount','Nutrients Unit']]
+    nutrients_list = nutrients.to_dict(orient='records')
+    nutrients_df = pd.DataFrame(nutrients_list)
+    print("hello")
+    return {
+        'Ingredients': ingredients_df,
+        'Nutrients': nutrients_df,
+        'Serving Size': user_serving_size,
+        'Dataset Serving Size':dataset_serving_size
+    }
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        serving_size = int(request.form['serving_size'])
+        
+        image = None
+        if 'image_url' in request.form and request.form['image_url']:
+            image_url = request.form['image_url']
+            image = Image.open(requests.get(image_url, stream=True).raw)
+        elif 'image_file' in request.files and request.files['image_file']:
+            image_file = request.files['image_file']
+            image = Image.open(image_file.stream)
+        if image:
+            vitB_16_224_prob = []
+            vitB_16_384_prob = []
+            vitB_32_224_prob = []
+            vitB_32_384_prob = []
+            
+            print("image Loaded")
+ 
+            
+            img = image.convert("RGB")
+            encoding_16_224 = feature_extractor_16_224(img, return_tensors="pt")
+            encoding_16_384 = feature_extractor_16_384(img, return_tensors="pt")
+
+            num_cores = multiprocessing.cpu_count()
+
+            print("Number of CPU cores:", num_cores)
+
+            items = [
+                ("p1", m1, encoding_16_224, vitB_16_224_prob),
+                ("p2", m2, encoding_16_384, vitB_16_384_prob),
+                ("p3", m3, encoding_16_224, vitB_32_224_prob),
+                ("p4", m4, encoding_16_384, vitB_32_384_prob),
+    
+            ]
+
+            with multiprocessing.Pool() as pool:
+                results = pool.starmap(probabilities_finder, items)
+                pool.close()
+                pool.join()
+            print(results)
+            vitB_16_224_prob = results[0]
+            vitB_16_384_prob = results[1]
+            vitB_32_224_prob = results[2]
+            vitB_32_384_prob= results[3]
+        
+
+            # # Define processes
+            # p1 = multiprocessing.Process(target=probabilities_finder, args=("p1", m1, encoding_16_384, vitB_16_384_1e_4_adam_prob_list))
+            # p2 = multiprocessing.Process(target=probabilities_finder, args=("p2", m2, encoding_16_384, vitB_16_384_2e_4_adam_prob_list))
+            # p3 = multiprocessing.Process(target=probabilities_finder, args=("p3", m3, encoding_16_384, vitB_16_384_1e_4_rms_prob_list))
+            # p4 = multiprocessing.Process(target=probabilities_finder, args=("p4", m4, encoding_16_384, vitB_16_384_2e_4_rms_prob_list))
+            # p5 = multiprocessing.Process(target=probabilities_finder, args=("p5", m5, encoding_16_224, vitB_16_224_1e_4_adam_prob_list))
+            # p6 = multiprocessing.Process(target=probabilities_finder, args=("p6", m6, encoding_16_224, vitB_16_224_2e_4_adam_prob_list))
+            # p7 = multiprocessing.Process(target=probabilities_finder, args=("p7", m7, encoding_16_224, vitB_16_224_1e_4_rms_prob_list))
+            # p8 = multiprocessing.Process(target=probabilities_finder, args=("p8", m8, encoding_16_224, vitB_16_224_2e_4_rms_prob_list))
+            # Start processes
+            # print("process Start")
+            # p1.start()
+            # p2.start()
+            # p3.start()
+            # p4.start()
+            # p5.start()
+            # p6.start()
+            # p7.start()
+            # p8.start()
+
+            # # Join processes to wait for them to finish
+            # print("Process Join")
+            # p1.join()
+            # p2.join()
+            # p3.join()
+            # p4.join()
+            # p5.join()
+            # p6.join()
+            # p7.join()
+            # p8.join()
+            # p1 = multiprocessing.Process(target=probabilities_finder, args=("ZaneHorrible/adam_VitB-p16-384-1e-4-batch_16_epoch_4_classes_24",image, vitB_16_384_1e_4_adam_prob_list))
+            # vitB_16_384_1e_4_adam_prob_list = probabilities_finder("ZaneHorrible/adam_VitB-p16-384-1e-4-batch_16_epoch_4_classes_24",image)
+            # vitB_16_384_2e_4_adam_prob_list= probabilities_finder("ZaneHorrible/google-vit-base-patch16-384-batch_16_epoch_4_classes_24",image)
+            # vitB_16_384_1e_4_rms_prob_list= probabilities_finder("ZaneHorrible/rmsProp_VitB-p16-384-1e-4-batch_16_epoch_4_classes_24",image)
+            # vitB_16_384_2e_4_rms_prob_list= probabilities_finder("ZaneHorrible/rmsProp_VitB-p16-384-2e-4-batch_16_epoch_4_classes_24",image)
+            # vitB_16_224_1e_4_adam_prob_list= probabilities_finder("ZaneHorrible/adam_VitB-p16-224-1e-4-batch_16_epoch_4_classes_24",image)
+            # vitB_16_224_2e_4_adam_prob_list= probabilities_finder("ZaneHorrible/google-vit-base-patch16-224-in21k-batch_16_epoch_4_classes_24",image)
+            # vitB_16_224_1e_4_rms_prob_list= probabilities_finder("ZaneHorrible/rmsprop_VitB-p16-224-1e-4-batch_16_epoch_4_classes_24",image)
+            # vitB_16_224_2e_4_rms_prob_list= probabilities_finder("ZaneHorrible/rmsProp_VitB-p16-384-2e-4-batch_16_epoch_4_classes_24",image)
+            import numpy as np
+
+        
+            vitB_16_224_prob_arr = np.stack([prob.numpy() for prob in vitB_16_224_prob])
+            vitB_16_384_prob_arr = np.stack([prob.numpy() for prob in vitB_16_384_prob])
+            vitB_32_224_prob_arr= np.stack([prob.numpy() for prob in vitB_32_224_prob])
+            vitB_32_384_prob_arr= np.stack([prob.numpy() for prob in vitB_32_384_prob])
+            print("ending stacking")
+            weight_rank = [1,2,3,4]
+            average_rank =[]
+            for i in weight_rank:
+                average_rank.append(i/10)
+            weights = average_rank
+            weighted_probs = (weights[1] * np.array(vitB_16_224_prob_arr)) + \
+                 (weights[3] * np.array(vitB_16_384_prob_arr)) + \
+                 (weights[0] * np.array(vitB_32_224_prob_arr)) + \
+                 (weights[2] * np.array(vitB_32_384_prob_arr))
+            final_preds = np.argmax(weighted_probs, axis=-1)
+            predicted_class = id2label[final_preds[0]]
+           
+            food_info = aggregate_food_info(df, predicted_class, serving_size)
+
+            return render_template('result.html', predicted_class=predicted_class, food_info=food_info ,serving_size = serving_size,dataset_serving_size=food_info['Dataset Serving Size'])
+        else:
+            return "No image provided", 400
+
+if __name__ == '__main__':
+    m1 = AutoModelForImageClassification.from_pretrained("ZaneHorrible/adam_VitB-p16-224-1e-4-batch_16_epoch_4_classes_24")
+    m2 =AutoModelForImageClassification.from_pretrained("ZaneHorrible/google-vit-base-patch16-384-batch_16_epoch_4_classes_24")
+    m3 = AutoModelForImageClassification.from_pretrained("ZaneHorrible/adam_ViTB-32-224-in21k-2e-4-batch_16_epoch_4_classes_24")
+    m4 = AutoModelForImageClassification.from_pretrained("ZaneHorrible/rmsProps_VitB-p32-384-1e-4-batch_32_epoch_4_classes_24")
+
+    print("Model loaded")
+    feature_extractor_16_224 = AutoFeatureExtractor.from_pretrained("ZaneHorrible/rmsprop_VitB-p16-224-1e-4-batch_16_epoch_4_classes_24")
+    feature_extractor_16_384 = AutoFeatureExtractor.from_pretrained("ZaneHorrible/rmsProp_VitB-p16-384-2e-4-batch_16_epoch_4_classes_24")
+    app.run(debug=True, port=5000, host='0.0.0.0')
+
